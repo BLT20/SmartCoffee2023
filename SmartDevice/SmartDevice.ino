02246238f602dbe1a11aef3dad67d98b8c746c64
Blaze <0592511@schoolsnet.act.edu.au>
2023-05-31 15:25:09 +1000

    Update SmartDevice.ino

--- SmartDevice/SmartDevice.ino
+++ SmartDevice/SmartDevice.ino
@@ -44,7 +44,16 @@ void loop() {
  @returns Check for cup
  */
 void userLocation () {
-
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+long duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+int distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
 }
 
 /*
@@ -53,7 +62,7 @@ void userLocation () {
  @returns Make coffee
  */
 void cupLocation () {
-
+int lineSensorValue = digitalRead(lineSensorPin);
 }
 
 /*
@@ -62,5 +71,5 @@ void cupLocation () {
  @returns Make coffee
  */
 void buttonPressed () {
-
+int crashSensorValue = digitalRead(crashSensor);
 }
